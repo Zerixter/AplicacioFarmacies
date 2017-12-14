aeb13b16c475b5a59e23e2e50d56e70846b79278
Hamza Saddouki <hsaddouki@gmail.com>
2017-12-14 16:43:51 +0100

    Added Model Builder

--- AplicacioFarmacies/Models/Context.cs
+++ AplicacioFarmacies/Models/Context.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 
 namespace AplicacioFarmacies.Models
 {
@@ -14,6 +16,23 @@ namespace AplicacioFarmacies.Models
         public DbSet<Provincia> Provincies { get; set; }
         public DbSet<Poblacio> Poblacions { get; set; }
         public DbSet<Farmacia> Farmacies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Poblacio>()
+                .HasRequired(r => r.Provincia)
+                .WithMany(m => m.Poblacions)
+                .HasForeignKey(k => k.IdProvincia)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<Farmacia>()
+                .HasRequired(r => r.Poblacio)
+                .WithMany(m => m.Farmacies)
+                .HasForeignKey(k => k.IdPoblacio)
+                .WillCascadeOnDelete(true);
+        }
     }
 }
 
