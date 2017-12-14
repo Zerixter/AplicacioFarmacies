85570ce64300f06db9d97ff53a5e7e218ecb015d
Hamza Saddouki <hsaddouki@gmail.com>
2017-12-14 16:55:47 +0100

    Added migration

---
+++ AplicacioFarmacies/Migrations/201712141548404_Migracio1.cs
@@ -0,0 +1,63 @@
+namespace AplicacioFarmacies.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migracio1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Farmacias",
+                c => new
+                    {
+                        IdFarmacia = c.Int(nullable: false, identity: true),
+                        CodiFarmacia = c.String(),
+                        NomFarmacia = c.String(),
+                        TipusVia = c.String(),
+                        Carrer = c.String(),
+                        NumeroVia = c.Int(nullable: false),
+                        NumeroTelefon = c.String(),
+                        AreaBasicaSalut = c.String(),
+                        IdPoblacio = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdFarmacia)
+                .ForeignKey("dbo.Poblacios", t => t.IdPoblacio, cascadeDelete: true)
+                .Index(t => t.IdPoblacio);
+            
+            CreateTable(
+                "dbo.Poblacios",
+                c => new
+                    {
+                        IdPoblacio = c.Int(nullable: false, identity: true),
+                        NomPoblacio = c.String(),
+                        CP = c.Int(nullable: false),
+                        IdProvincia = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdPoblacio)
+                .ForeignKey("dbo.Provincias", t => t.IdProvincia, cascadeDelete: true)
+                .Index(t => t.IdProvincia);
+            
+            CreateTable(
+                "dbo.Provincias",
+                c => new
+                    {
+                        IdProvincia = c.Int(nullable: false, identity: true),
+                        NomProvincia = c.String(),
+                    })
+                .PrimaryKey(t => t.IdProvincia);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Farmacias", "IdPoblacio", "dbo.Poblacios");
+            DropForeignKey("dbo.Poblacios", "IdProvincia", "dbo.Provincias");
+            DropIndex("dbo.Poblacios", new[] { "IdProvincia" });
+            DropIndex("dbo.Farmacias", new[] { "IdPoblacio" });
+            DropTable("dbo.Provincias");
+            DropTable("dbo.Poblacios");
+            DropTable("dbo.Farmacias");
+        }
+    }
+}
