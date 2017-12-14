b13e728b365bbc1e9478a1638c734b4d85b7416f
Hamza Saddouki <hsaddouki@gmail.com>
2017-12-14 16:05:41 +0100

    Afegit anotacions als Models

--- AplicacioFarmacies/Models/Farmacia.cs
+++ AplicacioFarmacies/Models/Farmacia.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +8,24 @@ namespace AplicacioFarmacies.Models
 {
     public class Farmacia
     {
+        [Key]
         public int IdFarmacia { get; set; }
+        [Display(Name = "Codi de la farmacia")]
         public string CodiFarmacia { get; set; }
+        [Display(Name = "Nom de la farmacia")]
         public string NomFarmacia { get; set; }
+        [Display(Name = "Tipus de via")]
         public string TipusVia { get; set; }
+        [Display(Name = "Carrer")]
         public string Carrer { get; set; }
+        [Display(Name = "Numero de via")]
         public int NumeroVia { get; set; }
+        [Display(Name = "Numero de telèfon")]
         public string NumeroTelefon { get; set; }
+        [Display(Name = "Area bàsica de salut")]
         public string AreaBasicaSalut { get; set; }
 
+        [Display(Name = "Població")]
         public int IdPoblacio { get; set; }
         public virtual Poblacio Poblacio { get; set; }
     }
--- AplicacioFarmacies/Models/Provincia.cs
+++ AplicacioFarmacies/Models/Provincia.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace AplicacioFarmacies.Models
 {
     public class Provincia
     {
+        [Key]
         public int IdProvincia { get; set; }
+        [Display(Name = "Nom de la provincia")]
         public string NomProvincia { get; set; }
 
         public virtual List<Poblacio> Poblacions { get; set } = new List<Poblacio>();
