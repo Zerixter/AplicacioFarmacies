a154ee18d15a352aa3d0001cd45940bd915a8861
Hamza Saddouki <hsaddouki@gmail.com>
2018-01-09 15:22:55 +0100

    Changes for eric

--- AplicacioFarmacies/Controllers/ComentarisController.cs
+++ AplicacioFarmacies/Controllers/ComentarisController.cs
@@ -43,6 +43,23 @@ namespace AplicacioFarmacies.Controllers
             return View();
         }
 
+        // GET: 
+        [HttpGet]
+        [Route("Comentaris/CreateByIdFarmacia/{id_farmacia:int}", Name = "Crear_Comentaris_Per_Farmacia")]
+        public ActionResult CreateByIdFarmacia(int id_farmacia)
+        {
+            Farmacia farmacia = db.Farmacies.Find(id_farmacia);
+            ViewBag.Farmacia = farmacia;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateByIdFarmacia([Bind(Include = "IdComentari, NomAutorComentari, ContingutComentariIdFarmacia")] Comentari comentari)
+        {
+            return View();
+        }
+       
+
         // POST: Comentaris/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
